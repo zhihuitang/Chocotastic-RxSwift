bd907c8642fbb327505111da38d3791c9e3b4b1b
Zhihui Tang <crafttang@gmail.com>
2017-01-24 15:53:41 +0100

    download from here: https://www.raywenderlich.com/138547/getting-started-with-rxswift-and-rxcocoa

---
+++ Pods/Target Support Files/Pods-Chocotastic/Pods-Chocotastic-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_ChocotasticVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_ChocotasticVersionString[];
+
